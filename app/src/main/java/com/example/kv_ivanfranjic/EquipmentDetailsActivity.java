6117ceddb16bd98f6571f8d57847b1a81dd9b887
Ivan Franjic <ifranjic099@gmail.com>
2021-01-20 20:39:36 +0100

    update

--- app/src/main/java/com/example/kv_ivanfranjic/EquipmentDetailsActivity.java
+++ app/src/main/java/com/example/kv_ivanfranjic/EquipmentDetailsActivity.java
@@ -3,18 +3,27 @@ package com.example.kv_ivanfranjic;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class EquipmentDetailsActivity extends AppCompatActivity {
 
@@ -23,6 +32,9 @@ public class EquipmentDetailsActivity extends AppCompatActivity {
     TextView equip_details_name, equip_details_description, equip_details_price;
     ElegantNumberButton equipquantity;
     String equipproductid = "";
+    Float totalequipmentproductprice;
+    Integer totalequipmentproductquantity = 0;
+    Float equipmentproductprice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +51,51 @@ public class EquipmentDetailsActivity extends AppCompatActivity {
         equip_details_price = (TextView) findViewById(R.id.equip_details_price);
 
         getEquipmentProductDetails(equipproductid);
+
+        addtocartequip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addingToCartList();
+            }
+        });
+    }
+
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd, MMM, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        /*SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());*/
+
+        DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart list");
+        totalequipmentproductquantity = Integer.parseInt(equipquantity.getNumber());
+        equipmentproductprice = Float.parseFloat(equip_details_price.getText().toString());
+        totalequipmentproductprice = totalequipmentproductquantity*equipmentproductprice;
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("id", equipproductid);
+        cartMap.put("name", equip_details_name.getText().toString());
+        cartMap.put("price", totalequipmentproductprice.toString());
+        cartMap.put("date", saveCurrentDate);
+        /*cartMap.put("time", saveCurrentTime);*/
+        cartMap.put("quantity", equipquantity.getNumber());
+
+        cartListRef.child("Products").child(equipproductid).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(EquipmentDetailsActivity.this, "Dodano u košaricu", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(EquipmentDetailsActivity.this, EquipmentActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
     }
 
     private void getEquipmentProductDetails(String equipproductid) {
@@ -51,7 +108,7 @@ public class EquipmentDetailsActivity extends AppCompatActivity {
                     Equipment equip = snapshot.getValue(Equipment.class);
                     equip_details_name.setText(equip.getNaziv());
                     equip_details_description.setText(equip.getNaziv());
-                    equip_details_price.setText("Cijena:"+" "+equip.getCijena()+" "+"kn");
+                    equip_details_price.setText(equip.getCijena());
                     Picasso.get().load(equip.getSlika()).into(equip_details_image);
                 }
             }
--- app/src/main/java/com/example/kv_ivanfranjic/FoodDetailsActivity.java
+++ app/src/main/java/com/example/kv_ivanfranjic/FoodDetailsActivity.java
@@ -3,13 +3,18 @@ package com.example.kv_ivanfranjic;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,12 +22,20 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.SimpleTimeZone;
+
 public class FoodDetailsActivity extends AppCompatActivity {
     Button addtocartfood;
     ImageView food_details_image;
     TextView food_details_name, food_details_description, food_details_price;
     ElegantNumberButton foodquantity;
     String foodproductid = "";
+    Float totalfoodproductprice;
+    Integer totalfoodproductquantity = 0;
+    Float foodproductprice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,50 @@ public class FoodDetailsActivity extends AppCompatActivity {
         food_details_price = (TextView) findViewById(R.id.food_details_price);
 
         getFoodProductDetails(foodproductid);
+        addtocartfood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addingToCartList();
+            }
+        });
+    }
+
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd, MMM, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        /*SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());*/
+
+        DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart list");
+        totalfoodproductquantity = Integer.parseInt(foodquantity.getNumber());
+        foodproductprice = Float.parseFloat(food_details_price.getText().toString());
+        totalfoodproductprice = totalfoodproductquantity*foodproductprice;
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("id", foodproductid);
+        cartMap.put("name", food_details_name.getText().toString());
+        cartMap.put("price", totalfoodproductprice.toString());
+        cartMap.put("date", saveCurrentDate);
+        /*cartMap.put("time", saveCurrentTime);*/
+        cartMap.put("quantity", foodquantity.getNumber());
+
+        cartListRef.child("Products").child(foodproductid).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(FoodDetailsActivity.this, "Dodano u košaricu", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(FoodDetailsActivity.this, FoodActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
     }
 
     private void getFoodProductDetails(String foodproductid) {
@@ -51,7 +108,7 @@ public class FoodDetailsActivity extends AppCompatActivity {
                     Food food = snapshot.getValue(Food.class);
                     food_details_name.setText(food.getNaziv());
                     food_details_description.setText(food.getNaziv());
-                    food_details_price.setText("Cijena:"+" "+food.getCijena()+" "+"kn");
+                    food_details_price.setText(food.getCijena());
                     Picasso.get().load(food.getSlika()).into(food_details_image);
                 }
             }
